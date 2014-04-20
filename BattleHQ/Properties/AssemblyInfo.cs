7ed4fd7b9cba1da18ddfc9177a3471f6e30f299e
John Gietzen <otac0n@gietzen.us>
2014-04-19 19:29:48 -0700

    Created the BattleHQ project.

---
+++ BattleHQ/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BattleHQ")]
+[assembly: AssemblyCopyright("Copyright © John Gietzen 2014")]
+[assembly: ComVisible(false)]
+[assembly: Guid("dfd5099b-a313-4bbc-b91b-2021a838ba84")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
