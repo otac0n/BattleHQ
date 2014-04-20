ca22f7e37f37c5a4d196352f9243537a854bc072
John Gietzen <otac0n@gietzen.us>
2014-04-20 13:22:12 -0700

    Added the test project.

---
+++ BattleHQ.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BattleHQ.Tests")]
+[assembly: AssemblyProduct("BattleHQ.Tests")]
+[assembly: AssemblyCopyright("Copyright © John Gietzen 2014")]
+[assembly: ComVisible(false)]
+[assembly: Guid("ca80e766-a385-43d7-b702-b6c707135c3b")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
