61b783112147fbb26d9413e85354afab3abd8654
John Gietzen <otac0n@gietzen.us>
2014-04-20 10:58:32 -0700

    Added the Spades library.

---
+++ BattleHQ.Spades/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyTitle("BattleHQ.Spades")]
+[assembly: AssemblyProduct("BattleHQ.Spades")]
+[assembly: AssemblyCopyright("Copyright © John Gietzen 2014")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
