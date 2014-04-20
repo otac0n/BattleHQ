9a0f3c8225e955bafd7a7429a3d41e99e57b99db
John Gietzen <otac0n@gietzen.us>
2014-04-20 09:09:13 -0700

    Added the 2048 library.

---
+++ BattleHQ.Tiles2048/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyTitle("BattleHQ.Tiles2048")]
+[assembly: AssemblyProduct("BattleHQ.Tiles2048")]
+[assembly: AssemblyCopyright("Copyright © John Gietzen 2014")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
