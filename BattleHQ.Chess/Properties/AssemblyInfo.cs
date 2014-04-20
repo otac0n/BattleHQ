fe4416604c9e4c84adbd0d8515159cd07b174424
John Gietzen <otac0n@gietzen.us>
2014-04-20 08:53:56 -0700

    Added the Chess library.

---
+++ BattleHQ.Chess/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyTitle("BattleHQ.Chess")]
+[assembly: AssemblyProduct("BattleHQ.Chess")]
+[assembly: AssemblyCopyright("Copyright © John Gietzen 2014")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
